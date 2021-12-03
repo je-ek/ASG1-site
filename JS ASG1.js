91e3902a3ec40fcf194c3ec860125d52617097c6
je-ek <s10222027@connect.np.edu.sg>
2021-12-03 22:10:36 +0800

    Img progress

---
+++ JS ASG1.js
@@ -0,0 +1,7 @@
+// typing animation script
+var typed = new Typed(".typing", {
+    strings: ["Teacher", "Student", "Employer", "Customer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
